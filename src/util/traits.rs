1117b70aa95e64fabc7358f99928517d7c7e1137
Derek Forgione <derekforgione@gmail.com>
2023-06-15 22:38:03 -0700

    Added doc comment.

--- src/util/traits.rs
+++ src/util/traits.rs
@@ -1,4 +1,14 @@
 
+/// The purpose of this trait is to tranform a type from itself into another type.
+/// It's for type coercion in places where type coercion is necesasary, such as with Iterators.
+/// ```rs
+/// pub fn mutate_iter<I: TypeTransform<MyType>, It: IntoIterator<Item = I>>(items: It) -> Vec<MyType> {
+/// 	let items = items.into_iter()
+/// 		// Mutate the iterator using I::transform
+/// 		.map(I::transform)
+/// 		.collect::<Vec<MyType>>()
+/// }
+/// ```
 pub trait TypeTransform<R> {
 	fn transform(self) -> R;
 }
