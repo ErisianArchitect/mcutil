9c9b612189216e261233804d8f450faa92828f43
Derek Forgione <derekforgione@gmail.com>
2023-07-01 08:15:36 -0700

    Added Optional trait

--- src/util/traits.rs
+++ src/util/traits.rs
@@ -18,3 +18,35 @@ impl<T, R: From<T>> TypeTransform<R> for T {
 		R::from(self)
 	}
 }
+
+/// Allows for passing optional parameters to a function.
+pub trait Optional<T> {
+	fn to_option(self) -> Option<T>;
+	fn or(self, default: T) -> T;
+}
+
+impl<T> Optional<T> for T {
+	fn to_option(self) -> Option<T> {
+		Some(self)
+	}
+
+	#[allow(unused)]
+	fn or(self, default: T) -> T  {
+		self
+	}
+	
+}
+
+impl<T> Optional<T> for Option<T> {
+	fn to_option(self) -> Option<T> {
+		self
+	}
+
+	fn or(self, default: T) -> T  {
+		if let Some(result) = self.to_option() {
+			result
+		} else {
+			default
+		}
+	}
+}
