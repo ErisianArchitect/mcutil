452270a9581298da2b421dc8d08d1614a94aa6d6
Derek Forgione <derekforgione@gmail.com>
2023-12-15 10:45:07 -0800

    Cleaned up unused code that was commented out.

--- src/world/io/mod.rs
+++ src/world/io/mod.rs
@@ -1,18 +1,1 @@
-// pub mod region_old;
-// pub mod regionfile;
-// pub mod regionbuilder;
 pub mod region;
-
-// impl<T: NbtWrite> Writable for T {
-//     fn write_to<W: Write>(&self, writer: &mut W) -> Result<usize,crate::McError> {
-//         use crate::nbt::io::*;
-// 		Ok(writer.write_nbt(self)?)
-//     }
-// }
-
-// impl<T: NbtRead> Readable for T {
-//     fn read_from<R: Read>(reader: &mut R) -> Result<Self,crate::McError> {
-//         use crate::nbt::io::*;
-// 		Ok(reader.read_nbt()?)
-//     }
-// }
