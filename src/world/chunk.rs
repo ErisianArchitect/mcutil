362bce65b4c1b04d2b61a793d33ac66a1588f809
Derek Forgione <derekforgione@gmail.com>
2023-12-15 10:45:27 -0800

    Updated to reflect BlockState changes.

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -3,29 +3,38 @@
 use std::collections::HashMap;
 use std::default;
 
+use super::blockstate::*;
+
 use crate::McError;
 use crate::nbt::*;
 use crate::nbt::io::*;
 use crate::nbt::tag::*;
+use crate::nbt::tagtype::*;
 
-pub struct BlockStateProperty {
-	name: String,
-	value: String,
+macro_rules! map_decoder {
+	($map:expr; $name:literal -> $type:ty) => {
+		<$type>::decode_nbt($map.remove($name).ok_or(McError::NotFoundInCompound($name.to_owned()))?)?
+	};
 }
 
-pub enum BlockStateProperties {
-	None,
-	Single {
-		name: String,
-		value: String,
-	},
-	Many(HashMap<String, String>),
-}
+// pub struct BlockStateProperty {
+// 	name: String,
+// 	value: String,
+// }
 
-pub struct BlockState {
-	name: String,
-	properties: BlockStateProperties,
-}
+// pub enum BlockStateProperties {
+// 	None,
+// 	Single {
+// 		name: String,
+// 		value: String,
+// 	},
+// 	Many(HashMap<String, String>),
+// }
+
+// pub struct BlockState {
+// 	name: String,
+// 	properties: BlockStateProperties,
+// }
 
 pub struct BlockStates {
 	palette: Vec<BlockState>,
@@ -59,21 +68,39 @@ pub struct HeightMaps {
 }
 
 pub struct Chunk {
+	/// DataVersion
 	data_version: i32,
+	/// xPos
 	x: i32,
+	/// yPos
 	y: i32,
+	/// zPos
 	z: i32,
+	/// LastUpdate
 	last_update: i64,
-	inhabited_time: i64,
+	/// Status
+	status: String,
+	/// sections
 	sections: ChunkSections,
-	block_entities: Vec<Map>,
+	/// block_entities
+	block_entities: Vec<BlockEntity>,
+	/// CarvingMasks
 	carving_masks: CarvingMasks,
+	/// HeightMaps
 	heightmaps: HeightMaps,
+	/// Lights
 	lights: ListTag,
+	/// Entities
 	entities: ListTag,
+	/// fluid_ticks
 	fluid_ticks: Vec<Map>,
+	/// block_ticks
 	block_ticks: Vec<Map>,
+	/// InhabitedTime
+	inhabited_time: i64,
+	/// PostProcessing
 	post_processing: Vec<ListTag>,
+	/// structures
 	structures: Map,
 }
 
@@ -94,6 +121,35 @@ pub struct CarvingMasks {
 	liquid: Vec<i8>,
 }
 
+pub struct BlockEntity {
+	id: String,
+	keep_packed: i8,
+	x: i32,
+	y: i32,
+	z: i32,
+	data: Map,
+}
+
+impl DecodeNbt for Vec<BlockEntity> {
+	type Error = McError;
+	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
+		if let Tag::List(ListTag::Compound(entities)) = nbt {
+			Ok(entities.into_iter().map(|mut entity| {
+				Ok(BlockEntity {
+					id: map_decoder!(entity; "id" -> String),
+					keep_packed: map_decoder!(entity; "keepPacked" -> i8),
+					x: map_decoder!(entity; "x" -> i32),
+					y: map_decoder!(entity; "y" -> i32),
+					z: map_decoder!(entity; "z" -> i32),
+					data: entity,
+				})
+			}).collect::<Result<Vec<BlockEntity>, McError>>()?)
+		} else {
+			Err(McError::NbtDecodeError)
+		}
+	}
+}
+
 pub fn get_coord_from_chunk_nbt(tag: &Tag) -> Option<(i32, i32)> {
 	if let Tag::Compound(compound) = tag {
 		if let Some(Tag::Int(x_pos)) = compound.get("xPos") {
@@ -105,26 +161,26 @@ pub fn get_coord_from_chunk_nbt(tag: &Tag) -> Option<(i32, i32)> {
 	None
 }
 
-impl DecodeNbt for Vec<ChunkSection> {
-	type Error = ();
+// impl DecodeNbt for Vec<ChunkSection> {
+// 	type Error = McError;
 
-	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
-		if let Tag::List(ListTag::Compound(compounds)) = nbt {
-			let sections = compounds.into_iter().map(|section| {
-				ChunkSection {
-					y: todo!(),
-					block_states: todo!(),
-					biomes: todo!(),
-					skylight: todo!(),
-					blocklight: todo!(),
-				}
-			}).collect();
-			Ok(sections)
-		} else {
-			Err(())
-		}
-	}
-}
+// 	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
+// 		if let Tag::List(ListTag::Compound(compounds)) = nbt {
+// 			let sections = compounds.into_iter().map(|section| {
+// 				ChunkSection {
+// 					y: todo!(),
+// 					block_states: todo!(),
+// 					biomes: todo!(),
+// 					skylight: todo!(),
+// 					blocklight: todo!(),
+// 				}
+// 			}).collect();
+// 			Ok(sections)
+// 		} else {
+// 			Err(McError::NbtDecodeError)
+// 		}
+// 	}
+// }
 
 impl DecodeNbt for ChunkSection {
 	type Error = McError;
@@ -160,24 +216,30 @@ impl DecodeNbt for ChunkSections {
 	}
 }
 
+pub fn load_chunk() -> Chunk {
+	todo!()
+}
+
+/*
+TODO: 	Make it so that chunks can be loaded directly from memory.
+		This would involve more complicated programming, but it would
+		give faster load times. I also need to make it so that there
+		is a World block registry to register blocks to.
+*/
 impl DecodeNbt for Chunk {
     type Error = McError;
 
     fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
-		macro_rules! map_decoder {
-			($map:expr; $name:literal -> $type:ty) => {
-				<$type>::decode_nbt($map.remove($name).ok_or(McError::NotFoundInCompound($name.to_owned()))?)?
-			};
-		}
+		
         if let Tag::Compound(mut map) = nbt {
 			Ok(Self {
-				data_version: map_decoder!(map; "data_version" -> i32),
+				data_version: map_decoder!(map; "DataVersion" -> i32),
 				x: map_decoder!(map; "xPos" -> i32),
 				y: map_decoder!(map; "yPos" -> i32),
 				z: map_decoder!(map; "zPos" -> i32),
-				last_update: map_decoder!(map; "last_update" -> i64),
-				sections: todo!(),
-				block_entities: todo!(),
+				last_update: map_decoder!(map; "LastUpdate" -> i64),
+				sections: map_decoder!(map; "sections" -> ChunkSections),
+				block_entities: map_decoder!(map; "block_entities" -> Vec<BlockEntity>),
 				carving_masks: todo!(),
 				heightmaps: todo!(),
 				lights: todo!(),
@@ -187,6 +249,7 @@ impl DecodeNbt for Chunk {
 				post_processing: todo!(),
 				structures: todo!(),
 				inhabited_time: todo!(),
+				status: todo!(),
 			})
 		} else {
 			Err(McError::NbtDecodeError)
