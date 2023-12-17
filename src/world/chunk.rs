5cca7745c134e1c7c2ea3a5cc9f53a5d69d0dab6
Derek Forgione <derekforgione@gmail.com>
2023-12-16 18:48:08 -0800

    Updated decoding logic. Still not finished.

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -6,12 +6,31 @@ use std::default;
 use super::blockstate::*;
 
 use crate::McError;
+use crate::math::bit::BitLength;
 use crate::nbt::*;
 use crate::nbt::io::*;
 use crate::nbt::tag::*;
 use crate::nbt::tagtype::*;
+use super::blockregistry::BlockRegistry;
+use super::world::*;
 
+/// This macro is used to remove an entry from a Map (usually HashMap or IndexMap)
+/// the item that is removed from the map is then decoded from the NBT
+/// into the requested type.
+/// ```rust,no_run
+/// let map: Map;
+/// let value: Byte = map_decoder!(map; "some tag" -> Byte);
+/// // In case the value might not exist.
+/// let option: Option<Byte> = map_decoder!(map; "some tag" -> Option<Byte>);
+/// ```
 macro_rules! map_decoder {
+	($map:expr; $name:literal -> Option<$type:ty>) => {
+		if let Some(tag) = $map.remove($name) {
+			Some(<$type>::decode_nbt(tag)?)
+		} else {
+			None
+		}
+	};
 	($map:expr; $name:literal -> $type:ty) => {
 		<$type>::decode_nbt($map.remove($name).ok_or(McError::NotFoundInCompound($name.to_owned()))?)?
 	};
@@ -106,10 +125,10 @@ pub struct Chunk {
 
 pub struct ChunkSection {
 	y: i8,
-	block_states: BlockStates,
-	biomes: Option<Biomes>,
-	skylight: Option<LightData>,
-	blocklight: Option<LightData>,
+	blocks: Option<Box<[u32]>>,
+	biomes: Option<Map>,
+	skylight: Option<Vec<i8>>,
+	blocklight: Option<Vec<i8>>,
 }
 
 pub struct ChunkSections {
@@ -182,42 +201,168 @@ pub fn get_coord_from_chunk_nbt(tag: &Tag) -> Option<(i32, i32)> {
 // 	}
 // }
 
-impl DecodeNbt for ChunkSection {
-	type Error = McError;
+// impl DecodeNbt for ChunkSection {
+// 	type Error = McError;
 
-	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
-		if let Tag::Compound(map) = nbt {
-			todo!()
-		} else {
-			Err(McError::NbtDecodeError)
-		}
+// 	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
+// 		if let Tag::Compound(map) = nbt {
+// 			todo!()
+// 		} else {
+// 			Err(McError::NbtDecodeError)
+// 		}
+// 	}
+// }
+
+// impl DecodeNbt for ChunkSections {
+// 	type Error = McError;
+
+// 	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
+// 		if let Tag::List(ListTag::Compound(sections)) = nbt {
+// 			Ok(Self {
+// 				sections: sections.into_iter().map(|section| {
+// 					Ok(ChunkSection {
+// 						y: todo!(),
+// 						block_states: todo!(),
+// 						biomes: todo!(),
+// 						skylight: todo!(),
+// 						blocklight: todo!(),
+// 					})
+// 				}).collect::<Result<Vec<ChunkSection>, McError>>()?,
+// 			})
+// 		} else {
+// 			Err(McError::NbtDecodeError)
+// 		}
+// 	}
+// }
+
+/*
+def extract_index(full_index, palette_size, block_states):
+	bitsize = max((palette_size - 1).bit_length(), 4)
+	#vpl = values per long
+	vpl = 64 // bitsize
+	mask = 2**bitsize-1
+	state_index = full_index // vpl
+	value_offset = (full_index % vpl) * bitsize
+	slot = int(block_states[state_index])
+	return (slot & (mask << value_offset)) >> value_offset
+
+	states_tag = section_tag['BlockStates']
+	palette = section_tag['Palette']
+
+	
+if palette is not None and states_tag is not None:
+	states = list()
+	blocks = numpy.ndarray(shape=(4096,),dtype=numpy.object_)
+	for v in palette.data:
+		name = v.Name.value
+		props = {}
+		if 'Properties' in v:
+			props = { k : val.value for k, val in v.Properties.data.items() }
+		states.append(blockregistry.register(name, props))
+	
+	for i in range(4096):
+		ind = extract_index(i, len(palette.data), states_tag.data)
+		blocks[i] = states[ind].unique_key
+*/
+
+pub fn decode_palette(palette: ListTag) -> Result<Vec<BlockState>, McError> {
+	if let ListTag::Compound(mut states) = palette {
+		states.into_iter().map(|mut state| {
+			let name = map_decoder!(state; "Name" -> String);
+			// The "Properties" tag may not exist.
+			let properties = if state.contains_key("Properties") {
+				let props = map_decoder!(state; "Properties" -> Map);
+				BlockProperties::from(props.into_iter().map(|(name, value)| {
+					if let Tag::String(value) = value {
+						Ok((name, value))
+					} else {
+						Err(McError::NbtDecodeError)
+					}
+				}).collect::<Result<Vec<(String, String)>, McError>>()?)
+			} else {
+				BlockProperties::none()
+			};
+			Ok(BlockState::new(name, properties))
+		}).collect::<Result<Vec<BlockState>, McError>>()
+	} else {
+		Err(McError::NbtDecodeError)
 	}
 }
 
-impl DecodeNbt for ChunkSections {
-	type Error = McError;
-
-	fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
-		if let Tag::List(ListTag::Compound(sections)) = nbt {
-			Ok(Self {
-				sections: sections.into_iter().map(|section| {
-					Ok(ChunkSection {
-						y: todo!(),
-						block_states: todo!(),
-						biomes: todo!(),
-						skylight: todo!(),
-						blocklight: todo!(),
-					})
-				}).collect::<Result<Vec<ChunkSection>, McError>>()?,
-			})
-		} else {
-			Err(McError::NbtDecodeError)
-		}
-	}
+pub fn extract_palette_index(index: usize, palette_size: usize, states: &[i64]) -> usize {
+	let bitsize = palette_size.bit_length().max(4);
+	let vpl = (64 / bitsize) as u64;
+	let mask = 2u64.pow(bitsize) - 1;
+	let state_index = index as u64 / vpl;
+	let value_offset = ((index as u64).rem_euclid(vpl)) as u32 * bitsize;
+	let slot = states[state_index as usize] as u64;
+	((slot & (mask << value_offset)) >> value_offset) as usize
 }
 
-pub fn load_chunk() -> Chunk {
+pub fn decode_section(block_registry: &mut BlockRegistry, mut section: Map) -> Result<ChunkSection, McError> {
+	let y = map_decoder!(section; "Y" -> Byte);
+	let biomes = map_decoder!(section; "biomes" -> Option<Map>);
+	let blocklight = map_decoder!(section; "BlockLight" -> Option<ByteArray>);
+	let skylight = map_decoder!(section; "SkyLight" -> Option<ByteArray>);
+
+	let mut block_states = map_decoder!(section; "block_states" -> Map);
+
+	let palette = decode_palette(map_decoder!(block_states; "palette" -> ListTag))?;
+	let palette = palette.into_iter()
+		.map(|state| {
+			block_registry.register(&state)
+		}).collect::<Vec<u32>>();
+	let block_data = if block_states.contains_key("data") {
+		let data = map_decoder!(block_states; "data" -> LongArray);
+		let data = (0..4096).into_iter().map(|full_index| {
+			let index = extract_palette_index(full_index, palette.len(), data.as_slice());
+			palette[index]
+		}).collect::<Vec<u32>>();
+		Some(data.into_boxed_slice())
+	} else {
+		None
+	};
+	// Now I need to transform the block_data and palette into registry IDs.
+	// The ending block_data should contain 4096 u32 values representing their IDs in
+	// the registry. So I need to register each BlockState in the palette with the
+	// registry, retrieving the ID. I think the appropriate way to do this would be
+	// to do an iterator map to the block_registry IDs.
 	todo!()
+}
+
+pub fn decode_chunk(block_registry: &mut BlockRegistry, nbt: Tag) -> Result<Chunk, McError> {
+	if let Tag::Compound(mut map) = nbt {
+		
+		if let Tag::List(ListTag::Compound(mut sections)) = map.remove("sections").ok_or(McError::NotFoundInCompound("sections".to_owned()))? {
+			let sections = sections.into_iter()
+				.map(|mut section| decode_section(block_registry, section))
+				.collect::<Result<Vec<ChunkSection>, McError>>()?;
+		} else {
+			return Err(McError::NbtDecodeError)
+		}
+		Ok(Chunk {
+			data_version: map_decoder!(map; "DataVersion" -> i32),
+			x: map_decoder!(map; "xPos" -> i32),
+			y: map_decoder!(map; "yPos" -> i32),
+			z: map_decoder!(map; "zPos" -> i32),
+			last_update: map_decoder!(map; "LastUpdate" -> i64),
+			// sections: map_decoder!(map; "sections" -> ChunkSections),
+			sections: todo!(),
+			block_entities: map_decoder!(map; "block_entities" -> Vec<BlockEntity>),
+			carving_masks: todo!(),
+			heightmaps: todo!(),
+			lights: todo!(),
+			entities: todo!(),
+			fluid_ticks: todo!(),
+			block_ticks: todo!(),
+			post_processing: todo!(),
+			structures: todo!(),
+			inhabited_time: todo!(),
+			status: todo!(),
+		})
+	} else {
+		Err(McError::NbtDecodeError)
+	}
 }
 
 /*
@@ -226,33 +371,33 @@ TODO: 	Make it so that chunks can be loaded directly from memory.
 		give faster load times. I also need to make it so that there
 		is a World block registry to register blocks to.
 */
-impl DecodeNbt for Chunk {
-    type Error = McError;
+// impl DecodeNbt for Chunk {
+//     type Error = McError;
 
-    fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
+//     fn decode_nbt(nbt: Tag) -> Result<Self, Self::Error> {
 		
-        if let Tag::Compound(mut map) = nbt {
-			Ok(Self {
-				data_version: map_decoder!(map; "DataVersion" -> i32),
-				x: map_decoder!(map; "xPos" -> i32),
-				y: map_decoder!(map; "yPos" -> i32),
-				z: map_decoder!(map; "zPos" -> i32),
-				last_update: map_decoder!(map; "LastUpdate" -> i64),
-				sections: map_decoder!(map; "sections" -> ChunkSections),
-				block_entities: map_decoder!(map; "block_entities" -> Vec<BlockEntity>),
-				carving_masks: todo!(),
-				heightmaps: todo!(),
-				lights: todo!(),
-				entities: todo!(),
-				fluid_ticks: todo!(),
-				block_ticks: todo!(),
-				post_processing: todo!(),
-				structures: todo!(),
-				inhabited_time: todo!(),
-				status: todo!(),
-			})
-		} else {
-			Err(McError::NbtDecodeError)
-		}
-    }
-}
+//         if let Tag::Compound(mut map) = nbt {
+// 			Ok(Self {
+// 				data_version: map_decoder!(map; "DataVersion" -> i32),
+// 				x: map_decoder!(map; "xPos" -> i32),
+// 				y: map_decoder!(map; "yPos" -> i32),
+// 				z: map_decoder!(map; "zPos" -> i32),
+// 				last_update: map_decoder!(map; "LastUpdate" -> i64),
+// 				sections: map_decoder!(map; "sections" -> ChunkSections),
+// 				block_entities: map_decoder!(map; "block_entities" -> Vec<BlockEntity>),
+// 				carving_masks: todo!(),
+// 				heightmaps: todo!(),
+// 				lights: todo!(),
+// 				entities: todo!(),
+// 				fluid_ticks: todo!(),
+// 				block_ticks: todo!(),
+// 				post_processing: todo!(),
+// 				structures: todo!(),
+// 				inhabited_time: todo!(),
+// 				status: todo!(),
+// 			})
+// 		} else {
+// 			Err(McError::NbtDecodeError)
+// 		}
+//     }
+// }
