c0313d7b3647b904aa5dada008be1e6745db6529
Derek Forgione <derekforgione@gmail.com>
2023-12-17 15:08:27 -0800

    Updates. Sorry for the lazy commit.

--- src/world/world.rs
+++ src/world/world.rs
@@ -3,23 +3,102 @@
 */
 #![allow(unused)]
 
-use std::{collections::HashMap, path::{PathBuf, Path}, marker::PhantomData};
-use crate::{McResult, McError};
+use std::{collections::HashMap, path::{PathBuf, Path}, marker::PhantomData, sync::{Arc, Mutex}};
+
+use crate::{McResult, McError, nbt::tag::NamedTag};
 
 use super::{
 	blockregistry::BlockRegistry,
 	blockstate::*,
-	chunk::Chunk,
-	io::region::RegionFile,
+	chunk::{Chunk, decode_chunk},
+	io::region::{
+		RegionFile,
+		coord::RegionCoord,
+		regionfile::{
+			RegionManager,
+		},
+	},
 };
 
-pub type CoordTup = (i32, i32);
-
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum Dimension {
 	Overworld,
 	Nether,
-	Other(String),
+	Other(u32),
+}
+
+impl Default for Dimension {
+	fn default() -> Self {
+		Dimension::Overworld
+	}
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Default)]
+pub struct WorldCoord {
+	pub x: i64,
+	pub z: i64,
+	pub dimension: Dimension,
+}
+
+impl WorldCoord {
+	pub fn new(x: i64, z: i64, dimension: Dimension) -> Self {
+		Self {
+			x,
+			z,
+			dimension
+		}
+	}
+
+	pub fn xz(self) -> (i64, i64) {
+		(
+			self.x,
+			self.z
+		)
+	}
+
+	pub fn region_coord(self) -> Self {
+		Self {
+			x: self.x / 32,
+			z: self.z / 32,
+			dimension: self.dimension,
+		}
+	}
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Default)]
+pub struct BlockCoord {
+	pub x: i64,
+	pub y: i64,
+	pub z: i64,
+	pub dimension: Dimension,
+}
+
+impl BlockCoord {
+	pub fn new(x: i64, y: i64, z: i64, dimension: Dimension) -> Self {
+		Self {
+			x,
+			y,
+			z,
+			dimension,
+		}
+	}
+
+	pub fn xyz(self) -> (i64, i64, i64) {
+		(
+			self.x,
+			self.y,
+			self.z,
+		)
+	}
+
+	pub fn subchunk_coord(self) -> Self {
+		BlockCoord {
+			x: self.x.rem_euclid(16),
+			y: self.y.rem_euclid(16),
+			z: self.z.rem_euclid(16),
+			dimension: self.dimension,
+		}
+	}
 }
 
 // 32x32 chunks
@@ -27,26 +106,122 @@ pub enum Dimension {
 
 // }
 
-pub trait ChunkManager<T: Sized>: Sized {
-	fn load_chunk(block_registry: &mut BlockRegistry, coord: CoordTup) -> McResult<()>;
-	fn save_chunk(block_registry: &BlockRegistry, coord: CoordTup) -> McResult<()>;
-	/// Do not handle the removing of chunks from JavaWorld
-	fn unload_chunk(coord: CoordTup) -> McResult<()>;
+pub trait ChunkManager: Sized {
+	fn create(directory: PathBuf) -> McResult<Self>;
+	fn load_chunk(&mut self, block_registry: &mut BlockRegistry, coord: WorldCoord) -> McResult<()>;
+	fn save_chunk(&self, block_registry: &BlockRegistry, coord: WorldCoord) -> McResult<()>;
+	fn save_all(&self, block_registry: &BlockRegistry) -> McResult<()>;
+	fn unload_chunk(&mut self, coord: WorldCoord) -> McResult<()>;
+
+	fn get_block_id(&self, block_registry: &BlockRegistry, coord: BlockCoord) -> McResult<u32>;
+	fn get_block_state(&self, block_registry: &BlockRegistry, coord: BlockCoord) -> McResult<BlockState>;
+	fn set_block_id(&self, block_registry: &mut BlockRegistry, coord: BlockCoord, id: u32) -> McResult<()>;
+	fn set_block_state(&self, block_registry: &mut BlockRegistry, coord: BlockCoord, state: BlockState) -> McResult<()>;
 }
 
-pub struct JavaWorld<Ct, M: ChunkManager<Ct>> {
+pub struct JavaChunkManager {
+	pub chunks: HashMap<WorldCoord, Arc<Mutex<Chunk>>>,
+	pub regions: HashMap<WorldCoord, Arc<Mutex<RegionFile>>>,
+	pub directory: PathBuf,
+}
+
+#[inline(always)]
+fn make_arcmutex<T>(value: T) -> Arc<Mutex<T>> {
+	Arc::new(Mutex::new(value))
+}
+
+impl ChunkManager for JavaChunkManager {
+	fn create(directory: PathBuf) -> McResult<Self> {
+		if directory.is_dir() {
+			Ok(Self {
+				directory,
+				chunks: HashMap::new(),
+				regions: HashMap::new(),
+			})
+		} else {
+			Err(McError::WorldDirectoryNotFound(directory))
+		}
+	}
+
+	fn load_chunk(&mut self, block_registry: &mut BlockRegistry, coord: WorldCoord) -> McResult<()> {
+		match coord.dimension {
+			Dimension::Overworld => {
+				let region_dir = self.directory.join("region");
+				let region_coord = coord.region_coord();
+				let (x, z) = region_coord.xz();
+				let file_path = format!("r.{x}.{z}.mca");
+				let file_path = region_dir.join(file_path);
+				let region_file = if !self.regions.contains_key(&region_coord) {
+					// Now to load the RegionFile.
+					let region_file = if file_path.is_file() {
+						make_arcmutex(RegionFile::open(file_path)?)
+					} else {
+						// If the file doesn't exist, we'll create a region file.
+						make_arcmutex(RegionFile::create(file_path)?)
+					};
+					self.regions.insert(region_coord, region_file.clone());
+					region_file
+				} else {
+					self.regions.get(&region_coord).unwrap().clone()
+				};
+				if let Ok(mut region) = region_file.lock() {
+					let chunk_tag: NamedTag = region.read_data(RegionCoord::from(region_coord.xz()))?;
+					let chunk = decode_chunk(block_registry, chunk_tag.tag)?;
+					self.chunks.insert(coord, make_arcmutex(chunk));
+				}
+				Ok(())
+			},
+			Dimension::Nether => todo!(),
+			Dimension::Other(_) => todo!(),
+		}
+	}
+
+	fn save_chunk(&self, block_registry: &BlockRegistry, coord: WorldCoord) -> McResult<()> {
+		todo!()
+	}
+
+	fn save_all(&self, block_registry: &BlockRegistry) -> McResult<()> {
+		todo!()
+	}
+
+	fn unload_chunk(&mut self, coord: WorldCoord) -> McResult<()> {
+		todo!()
+	}
+
+	fn get_block_id(&self, block_registry: &BlockRegistry, coord: BlockCoord) -> McResult<u32> {
+		todo!()
+	}
+
+	fn get_block_state(&self, block_registry: &BlockRegistry, coord: BlockCoord) -> McResult<BlockState> {
+		todo!()
+	}
+
+	fn set_block_id(&self, block_registry: &mut BlockRegistry, coord: BlockCoord, id: u32) -> McResult<()> {
+		todo!()
+	}
+
+	fn set_block_state(&self, block_registry: &mut BlockRegistry, coord: BlockCoord, state: BlockState) -> McResult<()> {
+		todo!()
+	}
+
+	
+}
+
+pub struct JavaWorld<M: ChunkManager> {
 	pub block_registry: BlockRegistry,
-	pub chunks: HashMap<(i32, i32, Dimension), Ct>,
-	pub regions: HashMap<(i32, i32, Dimension), RegionFile>,
+	pub chunk_manager: M,
 	directory: PathBuf,
-	_m: PhantomData<M>,
 }
 
-impl<Ct, M: ChunkManager<Ct>> JavaWorld<Ct, M> {
+impl<M: ChunkManager> JavaWorld<M> {
 	pub fn open<P: AsRef<Path>>(directory: P) -> McResult<Self> {
 		let directory = directory.as_ref().to_owned();
 		if directory.is_dir() {
-			todo!()
+			Ok(Self {
+				block_registry: BlockRegistry::new(),
+				chunk_manager: M::create(directory.clone())?,
+				directory,
+			})
 		} else {
 			Err(McError::WorldDirectoryNotFound(directory))
 		}
@@ -57,17 +232,21 @@ impl<Ct, M: ChunkManager<Ct>> JavaWorld<Ct, M> {
 	}
 }
 
-impl<M: ChunkManager<Chunk>> JavaWorld<Chunk, M> {
-	pub fn load_chunk(&mut self, coord: CoordTup) -> McResult<()> {
-		M::load_chunk(&mut self.block_registry, coord)
+impl<M: ChunkManager> JavaWorld<M> {
+	pub fn load_chunk(&mut self, coord: WorldCoord) -> McResult<()> {
+		self.chunk_manager.load_chunk(&mut self.block_registry, coord)
 	}
 
-	pub fn save_chunk(&mut self, coord: CoordTup) -> McResult<()> {
-		M::save_chunk(&mut self.block_registry, coord)
+	pub fn save_chunk(&mut self, coord: WorldCoord) -> McResult<()> {
+		self.chunk_manager.save_chunk(&mut self.block_registry, coord)
 	}
 
-	pub fn unload_chunk(&mut self, coord: CoordTup) -> McResult<()> {
-		M::unload_chunk(coord)
+	pub fn save_all(&self, block_registry: &BlockRegistry) -> McResult<()> {
+		self.chunk_manager.save_all(block_registry)
+	}
+
+	pub fn unload_chunk(&mut self, coord: WorldCoord) -> McResult<()> {
+		self.chunk_manager.unload_chunk(coord)
 	}
 }
 
