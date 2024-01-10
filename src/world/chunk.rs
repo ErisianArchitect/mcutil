d18f18f79d987258e997b65babaa16b4b6fbbf99
Derek Forgione <derekforgione@gmail.com>
2024-01-09 17:02:31 -0800

    Rearrange code.

--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -398,10 +398,9 @@ pub fn decode_section(block_registry: &mut BlockRegistry, mut section: Map) -> R
 		// to do an iterator map to the block_registry IDs.
 		let palette = decode_palette(map_decoder!(block_states; "palette" -> ListTag))?;
 		// Register blocks.
-		let palette = palette.iter()
-			.map(|state| {
-				block_registry.register(state)
-			}).collect::<Vec<u32>>();
+		let palette = palette.iter().map(|state| {
+			block_registry.register(state)
+		}).collect::<Vec<u32>>();
 		map_decoder!(block_states; "data" -> Option<LongArray>).map(|blocks| {
 			(0..4096).into_iter().map(|full_index| {
 				let index = extract_palette_index(full_index, palette.len(), &blocks);
